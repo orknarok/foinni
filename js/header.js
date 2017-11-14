4094302ad4d800f858838e8ca1c7d38c1efb9ec8
orknarok <vpstudent21@gmail.com>
2017-11-14 22:26:50 +0200

    Update 2017-11-14T20:26:46.255Z

--- js/header.js
+++
@@ -1,9 +0,0 @@
-// Add class to element on scroll (jQuery dependence)
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 0){
-        $('.header').addClass("header--isTransformed");
-    }
-    else{
-        $('.header').removeClass("header--isTransformed");
-    }
-});
